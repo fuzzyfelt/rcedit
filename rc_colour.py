4ce13367754587ad9f6472a77972beeefe1dd64d
Richard Felton <richard.felton@bbc.co.uk>
2015-05-20 15:07:28 +0100

    Initial commit of a script to change colours in a reference content file.

---
+++ rc_colour.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+
+import binascii
+import sys
+import string
+import re
+
+
+class byteFile:
+
+	def __init__(self, filename):
+
+		self.filename = filename
+		self.hexMode = False
+		data = open(filename, "rb").read()
+
+		regex = re.compile(r"@(\w+)\|(.*)\|$", re.IGNORECASE | re.MULTILINE)
+		self.rcDict = dict(regex.findall(data))
+
+	def setValue(self, key, value):
+		self.rcDict[key] = value
+
+	def keyExists(self, key):
+		return key in self.rcDict
+
+	def printContent(self):
+		for key, value in self.rcDict.iteritems():
+			print key, "\t", value
+
+	def writeFile(self):
+		#Cycle through the tags and write a new rc.file
+		output = open('out.rc', 'wb')
+		if(self.hexMode):
+			for key,value in self.rcDict.iteritems():
+				output.write(''.join(('@',key,'|',binascii.unhexlify(value),'|\n')))
+		else:
+			for key,value in self.rcDict.iteritems():
+				output.write(''.join(('@',key,'|',value,'|\n')))
+
+	def setHexMode(self):
+		self.hexMode = True
+		# Go through the dictionary and convert any values to hex
+		for key, value in self.rcDict.iteritems():
+			self.rcDict[key] = binascii.hexlify(value)
+
+# Get the file name
+rcData = byteFile(sys.argv[1])
+rcData.setHexMode();
+
+# List the items
+rcData.printContent();
+
+keyName = raw_input('Which key do you want to edit? ')
+
+if rcData.keyExists(keyName): 
+	print "Current value of ", keyName, " is ", rcData.rcDict[keyName]
+	newVal = raw_input('Enter new value: ')
+
+	rcData.setValue(keyName, newVal)
+
+	print 'Writing changes to file...'
+	rcData.writeFile()
+else:
+	print "Can't find that tag."
+
